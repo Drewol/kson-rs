f897f27f7670e1b537b102610c392006d9f83125
Drewol <emildraws@gmail.com>
2019-10-13 23:58:09 +0200

    BT loading and display

--- src/chart.rs
+++ src/chart.rs
@@ -1,113 +1,223 @@
-pub extern crate serde_json;
-pub extern crate serde;
+extern crate regex;
+extern crate serde;
+extern crate serde_json;
 
-use self::serde::{Serialize, Deserialize};
+use self::serde::{Deserialize, Serialize};
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+use std::str::Lines;
 
 #[derive(Serialize, Deserialize)]
 pub struct GraphSectionPoint {
-    ry : u32,
-    v : f64,
-    vf : f64,
-    a : f64,
-    b : f64
+    pub ry: u32,
+    pub v: f64,
+    pub vf: f64,
+    pub a: f64,
+    pub b: f64,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Interval {
-    y : u32,
-    l : u32
+    pub y: u32,
+    pub l: u32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct LaserSection {
-    y : u32,
-    v : Vec<GraphSectionPoint>
+    pub y: u32,
+    pub v: Vec<GraphSectionPoint>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct NoteInfo {
-    bt : [Vec<Interval>; 4],
-    fx : [Vec<Interval>; 2],
-    laser : [Vec<LaserSection>; 2]
+    pub bt: [Vec<Interval>; 4],
+    pub fx: [Vec<Interval>; 2],
+    pub laser: [Vec<LaserSection>; 2],
 }
 
 impl NoteInfo {
     fn new() -> NoteInfo {
         NoteInfo {
-            bt : [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
-            fx : [Vec::new(), Vec::new()],
-            laser : [Vec::new(), Vec::new()]
+            bt: [Vec::new(), Vec::new(), Vec::new(), Vec::new()],
+            fx: [Vec::new(), Vec::new()],
+            laser: [Vec::new(), Vec::new()],
         }
     }
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct DifficultyInfo {
-    name : String,
-    short_name : String,
-    idx : u8
+    pub name: String,
+    pub short_name: String,
+    pub idx: u8,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct MetaInfo {
-    title : String,
-    title_translit : String,
-    subtitle : String,
-    artist : String,
-    artist_translit : String,
-    chart_author : String,
-    difficulty : DifficultyInfo,
-    level : u8,
-    disp_bpm : String,
-    std_bpm : f64,
-    jacket_filename : String,
-    jacket_author : String,
-    information : String
+    pub title: String,
+    pub title_translit: String,
+    pub subtitle: String,
+    pub artist: String,
+    pub artist_translit: String,
+    pub chart_author: String,
+    pub difficulty: DifficultyInfo,
+    pub level: u8,
+    pub disp_bpm: String,
+    pub std_bpm: f64,
+    pub jacket_filename: String,
+    pub jacket_author: String,
+    pub information: String,
 }
 
 impl DifficultyInfo {
     fn new() -> DifficultyInfo {
         DifficultyInfo {
-            name : String::new(),
-            short_name : String::new(),
-            idx : 0
+            name: String::new(),
+            short_name: String::new(),
+            idx: 0,
         }
     }
 }
 
 impl MetaInfo {
-    fn new() -> MetaInfo
-    {
-         MetaInfo {
-                title : String::new(),
-                title_translit : String::new(),
-                subtitle : String::new(),
-                artist : String::new(),
-                artist_translit : String::new(),
-                chart_author : String::new(),
-                difficulty : DifficultyInfo::new(),
-                level : 1,
-                disp_bpm : String::new(),
-                std_bpm : std::f64::NAN,
-                jacket_filename : String::new(),
-                jacket_author : String::new(),
-                information : String::new()
+    fn new() -> MetaInfo {
+        MetaInfo {
+            title: String::new(),
+            title_translit: String::new(),
+            subtitle: String::new(),
+            artist: String::new(),
+            artist_translit: String::new(),
+            chart_author: String::new(),
+            difficulty: DifficultyInfo::new(),
+            level: 1,
+            disp_bpm: String::new(),
+            std_bpm: std::f64::NAN,
+            jacket_filename: String::new(),
+            jacket_author: String::new(),
+            information: String::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DoubleEvent {
+    pub y: u32,
+    pub v: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BeatInfo {
+    pub bpm: Vec<DoubleEvent>,
+    pub resolution: u32,
+}
+
+impl BeatInfo {
+    fn new() -> Self {
+        BeatInfo {
+            bpm: Vec::new(),
+            resolution: 240,
         }
     }
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Chart {
-    note_info : NoteInfo,
-    meta : MetaInfo
+    pub meta: MetaInfo,
+    pub note: NoteInfo,
+    pub beat: BeatInfo,
 }
 
 impl Chart {
-    pub fn new() -> Chart {
-        Chart
-        {
-            meta : MetaInfo::new(),
-            note_info : NoteInfo::new()
+    pub fn new() -> Self {
+        Chart {
+            meta: MetaInfo::new(),
+            note: NoteInfo::new(),
+            beat: BeatInfo::new(),
         }
+    }
+
+    pub fn from_ksh(path: String) -> Result<Chart, String> {
+        let mut new_chart = Chart::new();
+        let data = fs::read_to_string(path);
+        if data.is_err() {
+            match data.err() {
+                Some(error) => return Err(format!("{}", error)),
+                None => return Err(String::from("Unknown error.")),
+            }
+        }
+
+        let data = data.unwrap();
+        let data = &data[3..]; //Something about BOM(?)
+        let mut parts: Vec<&str> = data.split("\n--").collect();
+        let meta = (parts.first().unwrap()).lines();
+        for line in meta {
+            let line_data: Vec<&str> = line.split("=").collect();
+            if line_data.len() < 2 {
+                continue;
+            }
+            let value = String::from(line_data[1]);
+            match line_data[0] {
+                "title" => new_chart.meta.title = value,
+                "artist" => new_chart.meta.artist = value,
+                "effect" => new_chart.meta.chart_author = value,
+                "jacket" => new_chart.meta.jacket_filename = value,
+                "illustrator" => new_chart.meta.jacket_author = value,
+                "t" => {
+                    if !value.contains("-") {
+                        new_chart.beat.bpm.push(DoubleEvent {
+                            y: 0,
+                            v: value.parse().unwrap_or_else(|e| {
+                                println!("{}", e);
+                                panic!(e)
+                            }),
+                        })
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        parts.remove(0);
+        let mut y: u32 = 0;
+
+        let mut last_char: [char; 8] = ['0'; 8];
+        last_char[6] = '-';
+        last_char[7] = '-';
+
+        let mut long_y: [u32; 8] = [0; 8];
+
+        for measure in parts {
+            let measure_lines = measure.lines();
+            let note_regex = regex::Regex::new("[0-2]{4}\\|").unwrap();
+            let line_count = measure.lines().filter(|x| note_regex.is_match(x)).count() as u32;
+            if line_count == 0 {
+                continue;
+            }
+            let ticks_per_line = (new_chart.beat.resolution * 4) / line_count; //TODO: use time signature
+            for line in measure_lines {
+                if note_regex.is_match(line) {
+                    //read bt
+                    let chars: Vec<char> = line.chars().collect();
+                    for i in 0..4 {
+                        if chars[i] == '1' {
+                            new_chart.note.bt[i].push(Interval { y: y, l: 0 });
+                        } else if chars[i] == '2' && last_char[i] != '2' {
+                            long_y[i] = y;
+                        } else if chars[i] != '2' && last_char[i] == '2' {
+                            new_chart.note.bt[i].push(Interval {
+                                y: long_y[i],
+                                l: y - long_y[i],
+                            });
+                        }
+
+                        last_char[i] = chars[i];
+                    }
+
+                    y = y + ticks_per_line;
+                }
+            }
+        }
+        return Ok(new_chart);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,46 @@
 extern crate ggez;
-extern crate nfd;
 extern crate imgui;
+extern crate math;
+extern crate nfd;
 extern crate serde_json;
 
 use ggez::event;
+use ggez::event::MouseButton;
 use ggez::graphics;
 use ggez::nalgebra as na;
-use ggez::event::MouseButton;
 use ggez::{Context, GameResult};
+use math::round;
+use nfd::Response;
 mod chart;
 
 struct MainState {
-    redraw : bool,
-    pos_x : f32,
-    pos_y : f32,
-    chart : chart::Chart
+    redraw: bool,
+    chart: chart::Chart,
+    w: f32,
+    h: f32,
+    tick_height: f64,
+    track_width: f64,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0, pos_y: 0.0, redraw: false, chart: chart::Chart::new() };
+        let s = MainState {
+            w: 800.0,
+            h: 600.0,
+            redraw: false,
+            chart: chart::Chart::new(),
+            tick_height: 1.0,
+            track_width: 72.0,
+        };
         Ok(s)
+    }
+
+    fn tick_to_pos(&self, in_y: u32) -> (f64, f64) {
+        let tick = in_y as f64;
+        let x = round::floor((tick * self.tick_height) / self.h as f64, 0) * self.track_width * 2.0;
+        let y = ((tick * self.tick_height) as f64) % self.h as f64;
+        let y = self.h as f64 - y;
+        (x, y)
     }
 }
 
@@ -30,53 +50,126 @@ impl event::EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        if !self.redraw
-        {
+        if !self.redraw {
             ggez::timer::sleep(std::time::Duration::from_millis(15));
             return GameResult::Ok(());
         }
-        let fps = ggez::timer::fps;
-        let (w,h) = graphics::size(ctx);
-        println!("{}x{}",w,h);
-        println!("{} FPS", fps(ctx));
-        let t:[f32; 16] = graphics::transform(ctx).into();
-        println!("t:");
-        for x in t.iter() {
-            print!("{},", x);            
-        }
-        println!("", );
-        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-        let circle = graphics::Mesh::new_circle(
+        graphics::clear(ctx, graphics::BLACK);
+
+        //draw track
+        let track = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            na::Point2::new(0.0, 0.0),
-            100.0,
-            22.1,
+            graphics::Rect {
+                x: 0.0,
+                y: 0.0,
+                w: self.track_width as f32,
+                h: self.h,
+            },
+            [0.2, 0.2, 0.2, 1.0].into(),
+        )?;
+
+        let track_count = 1 + (0.5 * self.w / self.track_width as f32) as u32;
+        for i in 0..track_count {
+            graphics::draw(
+                ctx,
+                &track,
+                (na::Point2::new(
+                    (self.track_width / 2.0 + i as f64 * self.track_width * 2.0) as f32,
+                    0.0,
+                ),),
+            )?;
+        }
+
+        //draw notes
+        let note_builder = &mut graphics::MeshBuilder::new();
+        let note = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            graphics::Rect {
+                x: self.track_width as f32 / 2.0,
+                y: 0.0,
+                w: self.track_width as f32 / 6.0 - 2.0,
+                h: -2.0,
+            },
             graphics::WHITE,
         )?;
-        graphics::draw(ctx, &circle, (na::Point2::new(self.pos_x, self.pos_y),))?;
+        for i in 0..4 {
+            for n in &self.chart.note.bt[i] {
+                if n.l == 0 {
+                    let (x, y) = self.tick_to_pos(n.y);
+                    if x > self.w as f64 + self.track_width * 2.0 {
+                        break;
+                    }
+                    let x = (x + (i + 1) as f64 * self.track_width / 6.0) as f32 - 1.0
+                        + self.track_width as f32 / 2.0;
+                    let y = y as f32;
+                    let w = self.track_width as f32 / 6.0 - 2.0;
+                    let h = -2.0;
+
+                    note_builder.rectangle(
+                        graphics::DrawMode::fill(),
+                        [x, y, w, h].into(),
+                        graphics::WHITE,
+                    );
+                } else {
+                }
+            }
+        }
+
+        let note_mesh = note_builder.build(ctx).unwrap();
+        graphics::draw(ctx, &note_mesh, (na::Point2::new(0.0, 0.0),))?;
 
         graphics::present(ctx)?;
+        println!("{}fps", ggez::timer::fps(ctx));
         self.redraw = false;
         Ok(())
     }
-    fn mouse_button_down_event(&mut self, _ctx: &mut Context, _button: MouseButton, _x: f32, _y: f32) {
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
         self.redraw = true;
-        self.pos_x = _x;
-        self.pos_y = _y;
-
-        let data = serde_json::to_string_pretty(&self.chart).unwrap();
+        let data = serde_json::to_string(&self.chart).unwrap();
         println!("Serialized = {}", data);
-
     }
 
     fn resize_event(&mut self, ctx: &mut Context, w: f32, h: f32) {
         self.redraw = true;
-        graphics::set_screen_coordinates(ctx, graphics::Rect {x:0.0,y:0.0,w:w,h:h});
+        graphics::set_screen_coordinates(
+            ctx,
+            graphics::Rect {
+                x: 0.0,
+                y: 0.0,
+                w: w,
+                h: h,
+            },
+        );
+        self.w = w;
+        self.h = h;
+        self.tick_height = h as f64 / (self.chart.beat.resolution as f64 * 16.0);
     }
 }
 
 pub fn main() -> GameResult {
+    let state = &mut MainState::new()?;
+
+    let diagResult = nfd::dialog().filter("ksh").open().unwrap_or_else(|e| {
+        println!("{}", e);
+        panic!(e);
+    });
+
+    match diagResult {
+        nfd::Response::Okay(file_path) => {
+            state.chart = chart::Chart::from_ksh(file_path).unwrap_or_else(|e| {
+                panic!(e);
+            })
+        }
+        _ => (),
+    }
 
     let win_setup = ggez::conf::WindowSetup {
         title: "USC Editor".to_owned(),
@@ -99,10 +192,11 @@ pub fn main() -> GameResult {
         resizable: true,
     };
 
-    let cb = ggez::ContextBuilder::new("usc-editor", "Drewol").window_setup(win_setup).window_mode(mode);
+    let cb = ggez::ContextBuilder::new("usc-editor", "Drewol")
+        .window_setup(win_setup)
+        .window_mode(mode);
 
     let (ctx, event_loop) = &mut cb.build()?;
 
-    let state = &mut MainState::new()?;
     event::run(ctx, event_loop, state)
 }
