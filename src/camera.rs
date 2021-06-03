f92d26293eedf3359bd5f7e41a86dbbccda78d79
Drewol <emildraws@gmail.com>
2021-06-03 17:46:08 +0200

    Missed file in previous commit

--- src/camera.rs
+++ src/camera.rs
@@ -40,20 +40,3 @@ pub struct CamGraphs {
     pub rotation_z_lane: GraphVec,
     pub rotation_z_jdgline: GraphVec,
 }
-
-pub fn parse_ksh_zoom_values(data: &str) -> Result<(f64, Option<f64>), Box<dyn Error>> {
-    let (v, vf): (f64, Option<f64>) = {
-        if data.contains(';') {
-            let mut values = data.split(';');
-            (
-                values.next().unwrap_or("0").parse()?,
-                values.next().map(|vf| vf.parse::<f64>().unwrap_or(0.)),
-            )
-        } else {
-            (data.parse()?, None)
-        }
-    };
-    let v = v / 100.0;
-    let vf = vf.map(|val| val / 100.0);
-    Ok((v, vf))
-}
