c512dd2a0129d6fba63eb938bdee34febd0d8e43
Basil <basil@basil.cafe>
2023-09-06 13:48:44 -0600

    [game] Store config in user config directory

--- game/src/main.rs
+++ game/src/main.rs
@@ -86,24 +86,12 @@ macro_rules! block_on {
 
 pub type RuscMixer = Arc<DynamicMixerController<f32>>;
 
-//TODO: Move to platform files
-#[cfg(target_os = "windows")]
 pub fn default_game_dir() -> PathBuf {
-    let mut game_dir = directories::UserDirs::new()
+    let mut game_dir = directories::BaseDirs::new()
         .expect("Failed to get directories")
-        .document_dir()
-        .expect("Failed to get documents directory")
+        .config_dir()
         .to_path_buf();
-    game_dir.push("USC");
-    game_dir
-}
-#[cfg(not(target_os = "windows"))]
-pub fn default_game_dir() -> PathBuf {
-    let mut game_dir = directories::UserDirs::new()
-        .expect("Failed to get directories")
-        .home_dir()
-        .to_path_buf();
-    game_dir.push(".usc");
+    game_dir.push("usc");
     game_dir
 }
 
