aa1db5cecdd94dbf2d9cd829ef9b55fc7b03a94d
Drewol <emildraws@gmail.com>
2025-07-25 11:33:27 +0200

    Fix android build

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -11,8 +11,8 @@ fn android_main(aapp: AndroidApp) {
     use winit::platform::android::EventLoopBuilderExtAndroid;
 
     android_logger::init_once(android_logger::Config::default().with_max_level(LevelFilter::Info));
-    INSTALL_DIR_OVERRIDE.set(aapp.internal_data_path().expect("No internal data path"));
-    GAME_DIR_OVERRIDE.set(aapp.internal_data_path().expect("No external data path"));
+    installer::INSTALL_DIR_OVERRIDE.set(aapp.internal_data_path().expect("No internal data path"));
+    installer::GAME_DIR_OVERRIDE.set(aapp.internal_data_path().expect("No external data path"));
     let event_loop = winit::event_loop::EventLoop::<UscInputEvent>::with_user_event()
         .with_android_app(aapp)
         .build()
